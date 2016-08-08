07c60f4cd25f67ea23835df8c92dbd9c87b86b4d
lc <ziyu0123456789@gmail.com>
2016-08-08 21:36:40 +0800

    引入requests beautifulsoup4

---
+++ src/CaptchaPython.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+验证码识别入口类
+
+"""
+
+if __name__ == '__main__':
+
+    """
+    模拟豆瓣首页来获取验证码
+    """
+    # TODO 下载验证码
+
+    # TODO 去噪
+
+    # TODO 识别
+
+
+    pass
